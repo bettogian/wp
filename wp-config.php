1e14f9337931a3f7c49c9f7b9ae75251b7209653
bettogian <hipopo@gmail.com>
2021-06-29 16:51:52 -0500

    nuevos updates

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'artek_wp_local' );
+define( 'DB_NAME', '######' );
 
 /** MySQL database username */
-define( 'DB_USER', 'artek_user' );
+define( 'DB_USER', '######' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '#betto!' );
+define( 'DB_PASSWORD', '#####' );
 
 /** MySQL hostname */
-define( 'DB_HOST', '192.254.250.162' );
+define( 'DB_HOST', '####' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
